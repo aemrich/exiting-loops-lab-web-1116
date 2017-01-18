86dbf5d8f1facd61949260904da7578b529470fc
aemrich <adam.emrich@gmail.com>
2017-01-18 17:36:59 -0500

    done

---
+++ index.js
@@ -0,0 +1,31 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i ++) {
+if (array[i] !== stopValue) {
+  array[i] = changeValue
+} else if (array[i] === stopValue) {
+  return array
+  break
+}
+
+  }
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i ++) {
+    if (array[i] !== skipValue) {
+      array[i] = changeValue
+    } else if (array[i] === skipValue) {
+      continue
+    }
+  }
+  return array
+}
+
+function findBy(array, findFn) {
+  for (let i = 0; i < array.length; i ++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
+  return null
+}
